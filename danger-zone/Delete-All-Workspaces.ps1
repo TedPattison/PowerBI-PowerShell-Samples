3f8b36f9ba74e16777c77f6ccca6166646e81eab
Ted Pattison <ted@tedpattison.net>
2020-06-19 15:30:20 -0400

    Updates

--- danger-zone/Delete-All-Workspaces.ps1
+++ danger-zone/Delete-All-Workspaces.ps1
@@ -1,18 +1,4 @@
-cls
-# log into Azure AD
-$userName = "tedp@mstdev2020.onMicrosoft.com"
-$password = "Pa`$`$word!"
-
-$securePassword = ConvertTo-SecureString –String $password –AsPlainText -Force
-$credential = New-Object –TypeName System.Management.Automation.PSCredential `
-                         –ArgumentList $userName, $securePassword
-
-$workspaceName = "Bluto"
-$importName = "Northwind111"
-$pbixFilePath = "C:\DevProjects\Northwind.pbix"
-
-$conn = Connect-PowerBIServiceAccount -Environment Public -Credential $credential
-
+Connect-PowerBIServiceAccount
 
 $workspaces = Get-PowerBIWorkspace
 
@@ -22,4 +8,5 @@ foreach($workspace in $workspaces){
   Invoke-PowerBIRestMethod -Method Delete -Url $url
 }
 
+# check to ensure all workspaces have been deleted
 Get-PowerBIWorkspace
