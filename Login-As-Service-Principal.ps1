ebd39e76f2684373e4be59d6ba23b1e0dd6738a8
Ted Pattison <ted@tedpattison.net>
2020-06-19 14:55:21 -0400

    Updates

--- Login-As-Service-Principal.ps1
+++ Login-As-Service-Principal.ps1
@@ -3,12 +3,11 @@ $tenantId = "ADD_TENANT_ID_HERE"
 $applictionId = "ADD_APPLICATION_ID_HERE"
 $applicationSecret = "ADD_APPLICATION_SECRET_HERE"
 
-$SecuredApplicationSecret = ConvertTo-SecureString ï¿½String $applicationSecret ï¿½AsPlainText -Force
-$credential = New-Object ï¿½TypeName System.Management.Automation.PSCredential `
-                         ï¿½ArgumentList $applictionId, $SecuredApplicationSecret
+$SecuredApplicationSecret = ConvertTo-SecureString –String $applicationSecret –AsPlainText -Force
+$credential = New-Object –TypeName System.Management.Automation.PSCredential `
+                         –ArgumentList $applictionId, $SecuredApplicationSecret
 
-
-# Disconnect-PowerBIServiceAccount
+Disconnect-PowerBIServiceAccount
 $sp = Connect-PowerBIServiceAccount -Environment Public -ServicePrincipal  -Credential $credential -Tenant $tenantId
 
 $AppId = $sp.UserName
